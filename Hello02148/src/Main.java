1be9e31bddb52b446193d746c758595d479b4a37
Kasper <Kasperlsorensen@gmail.com>
2016-01-07 13:47:01 +0100

    Sorting into text, pics and others folders.

--- Hello02148/src/Main.java
+++ Hello02148/src/Main.java
@@ -8,60 +8,72 @@ import com.dropbox.core.DbxRequestConfig;
 public class Main {
 
 	// Some global variables
-		//PUSH TEST
-		static DbxAppInfo appInfo;
-		static DbxRequestConfig config;
-		static DbxClient client;
+	// PUSH TEST
+	static DbxAppInfo appInfo;
+	static DbxRequestConfig config;
+	static DbxClient client;
 
-		public static void main(String[] args) throws IOException, DbxException {
+	public static void main(String[] args) throws IOException, DbxException {
 
-			// Get your app key and secret from the Dropbox developers website and
-			// insert them below
-			final String APP_KEY = "6gm4dggg0pm4qd3";
-			final String APP_SECRET = "mnm27mqi749672i";
-			appInfo = new DbxAppInfo(APP_KEY, APP_SECRET);
-			config = new DbxRequestConfig("JavaTutorial/1.0", Locale.getDefault().toString());
+		// Get your app key and secret from the Dropbox developers website and
+		// insert them below
+		final String APP_KEY = "6gm4dggg0pm4qd3";
+		final String APP_SECRET = "mnm27mqi749672i";
+		appInfo = new DbxAppInfo(APP_KEY, APP_SECRET);
+		config = new DbxRequestConfig("JavaTutorial/1.0", Locale.getDefault().toString());
 
-			// Insert here the access token from your app.
-			final String accessToken = "2IXvlsnWAFAAAAAAAAAAEPhoKJM-eyCMjv3hmLJncYB_x536trI0mGHg3U-OIYep";
+		// Insert here the access token from your app.
+		final String accessToken = "2IXvlsnWAFAAAAAAAAAAEPhoKJM-eyCMjv3hmLJncYB_x536trI0mGHg3U-OIYep";
 
-			client = new DbxClient(config, accessToken);
+		client = new DbxClient(config, accessToken);
 
-			// Name of the shared folder to be used as shared space
-			final String space = "/space/collage";
+		// Name of the shared folder to be used as shared space
+		final String space = "/space/collage";
 
-			// Two tuple templates for the main loop of the app
-			Template t1;
-			Template t2;
+		// Two tuple templates for the main loop of the app
+		Template t1;
+		Template t2;
 
-			// Main loop that processes files in the shared space
-			while (true) {
+		// Main loop that processes files in the shared space
+		while (true) {
 
-				// Template t1 represents "any file in the space folder"
-				t1 = new Template(space, "?", "?", null);
-				System.out.println("Looking for some file...");
-				t1.get(client);
-				System.out.println("Found file " + t1.name + "." + t1.ext);
+			// Template t1 represents "any file in the space folder"
+			t1 = new Template(space, "?", "?", null);
+			System.out.println("Looking for some file...");
+			t1.get(client);
+			System.out.println("Found file " + t1.name + "." + t1.ext);
 
-				// t2 is a tuple representing the previously retreived file in a new
-				// folder (named after the extension of the file)
-				if (t1.supported){
-					t2 = new Template(space + "/pics/", t1.name, t1.ext, t1.content);
-					System.out.println("Putting the file in the picture subfolder");
-					t2.put(client);
-				} else {
-					t2 = new Template(space + "/others/", t1.name, t1.ext, t1.content);
-					System.out.println("Putting the file in the 'other' subfolder...");
-					t2.put(client);
-				}
-				
-
+			// t2 is a tuple representing the previously retreived file in a new
+			// folder (named after the extension of the file)
+			if (isPicture(t1.type)) {
+				t2 = new Template(space + "/pics/", t1.name, t1.ext, t1.content);
+				System.out.println("Putting the file in the 'picture' subfolder");
+			} else if (isText(t1.type)) {
+				t2 = new Template(space + "/text/", t1.name, t1.ext, t1.content);
+				System.out.println("Putting the file in the 'text' subfolder");
+			} else {
+				t2 = new Template(space + "/others/", t1.name, t1.ext, t1.content);
+				System.out.println("Putting the file in the 'others' subfolder...");
 			}
+			t2.put(client);
 
 		}
-		
-		public static DbxClient getClient() {
-			return client;
+
+	}
+
+	public static boolean isPicture(String s) {
+		if (s.equals("PNG") || s.equals("JPG") || s.equals("JPEG") || s.equals("png") || s.equals("jpg")
+				|| s.equals("jpeg")) {
+			return true;
 		}
-	
+		return false;
+	}
+
+	public static boolean isText(String s) {
+		if (s.equals("txt") || s.equals("TXT")) {
+			return true;
+		}
+		return false;
+	}
+
 }
--- Hello02148/src/Template.java
+++ Hello02148/src/Template.java
@@ -7,101 +7,90 @@ import com.dropbox.core.DbxException;
 import com.dropbox.core.DbxWriteMode;
 
 public class Template {
-	
-		String path; // File folder
-		String name; // File name (without extension), "?" denotes any value
-		String ext; // File extension, "?" denotes any value
-		byte[] content; // File content (as byte array)
-		boolean supported;
 
-		public Template(String p, String n, String e, byte[] c) {
-			this.path = p;
-			this.name = n;
-			this.ext = e;
-			this.content = c;
-			this.supported = false;
-			//this.client = Main.getClient();
-		}
+	String path; // File folder
+	String name; // File name (without extension), "?" denotes any value
+	String ext; // File extension, "?" denotes any value
+	byte[] content; // File content (as byte array)
+	String type;
 
-		// Blocking operation to check the existence of a file matching a
-		// template
-		// The template is updated with the matched values for the tuple
-		// arguments that were binders (i.e. "?")
-		public void qry(DbxClient client) throws DbxException, IOException {
-			read(client, false);
-		}
+	public Template(String p, String n, String e, byte[] c) {
+		this.path = p;
+		this.name = n;
+		this.ext = e;
+		this.content = c;
+		this.type = null;
+		// this.client = Main.getClient();
+	}
 
-		// As qry() but the file is removed from the space
-		public void get(DbxClient client) throws DbxException, IOException {
-			read(client, true);
-		}
+	// Blocking operation to check the existence of a file matching a
+	// template
+	// The template is updated with the matched values for the tuple
+	// arguments that were binders (i.e. "?")
+	public void qry(DbxClient client) throws DbxException, IOException {
+		read(client, false);
+	}
 
-		// Auxiliary function that implements the both qry() and get()
-		public void read(DbxClient client, boolean delete) throws DbxException, IOException {
-			DbxEntry.WithChildren listing;
-			String name_aux = name;
-			String ext_aux = ext;
+	// As qry() but the file is removed from the space
+	public void get(DbxClient client) throws DbxException, IOException {
+		read(client, true);
+	}
 
-			// Repeat until one file/tuple is found
-			while (true) {
-				listing = client.getMetadataWithChildren(path);
-				for (DbxEntry child : listing.children) {
-					if (name == "?") {
-						int j = child.name.lastIndexOf('.');
-						if (j == -1)
-							continue;
-						name_aux = child.name.substring(0, j);
-					}
-					if (ext == "?") {
-						int j = child.name.lastIndexOf('.');
-						if (j == -1)
-							continue;
-						if (supportedTypes(child.name.substring(j+1))){
-							supported = true;
-							ext_aux = child.name.substring(j + 1);
-						} else {
-							ext_aux = child.name.substring(j + 1);
-						}
-						
-					}
-					if (child.name.equals(name_aux + "." + ext_aux)) {
-						name = name_aux;
-						ext = ext_aux;
-						ByteArrayOutputStream out = new ByteArrayOutputStream();
-						client.getFile(child.path, null, out);
-						content = out.toByteArray();
-						out.close();
-						// Delete the file if this is a get()
-						if (delete) {
-							client.delete(child.path);
-						}
-						return;
-					}
+	// Auxiliary function that implements the both qry() and get()
+	public void read(DbxClient client, boolean delete) throws DbxException, IOException {
+		DbxEntry.WithChildren listing;
+		String name_aux = name;
+		String ext_aux = ext;
+
+		// Repeat until one file/tuple is found
+		while (true) {
+			listing = client.getMetadataWithChildren(path);
+			for (DbxEntry child : listing.children) {
+				if (name == "?") {
+					int j = child.name.lastIndexOf('.');
+					if (j == -1)
+						continue;
+					name_aux = child.name.substring(0, j);
 				}
-				// Simple implementation based on busy-wait
-				// We hence insert a delay of 10 seconds to minimise unsucessful
-				// checks
-				System.out.println("Blocking operation (qry/get) was unsucessful, sleeping for a while...");
-				try {
-					Thread.sleep(10000);
-				} catch (InterruptedException ex) {
-					Thread.currentThread().interrupt();
+				if (ext == "?") {
+					int j = child.name.lastIndexOf('.');
+					if (j == -1) {
+						continue;
+					}
+					type = child.name.substring(j + 1);
+					ext_aux = type;
+
+				}
+				if (child.name.equals(name_aux + "." + ext_aux)) {
+					name = name_aux;
+					ext = ext_aux;
+					ByteArrayOutputStream out = new ByteArrayOutputStream();
+					client.getFile(child.path, null, out);
+					content = out.toByteArray();
+					out.close();
+					// Delete the file if this is a get()
+					if (delete) {
+						client.delete(child.path);
+					}
+					return;
 				}
 			}
-		}
-
-		public void put(DbxClient client) throws DbxException, IOException {
-			ByteArrayInputStream in = new ByteArrayInputStream(content);
-			
-			client.uploadFile(path + name + "." + ext, DbxWriteMode.add(), -1, in);
-			return;
-		}
-		
-		public boolean supportedTypes(String s){
-			if (s.equals("PNG") || s.equals("JPG") || s.equals("JPEG") 
-					|| s.equals("png") || s.equals("jpg") || s.equals("jpeg")){
-				return true;
+			// Simple implementation based on busy-wait
+			// We hence insert a delay of 10 seconds to minimise unsucessful
+			// checks
+			System.out.println("Blocking operation (qry/get) was unsucessful, sleeping for a while...");
+			try {
+				Thread.sleep(10000);
+			} catch (InterruptedException ex) {
+				Thread.currentThread().interrupt();
 			}
-			return false;
 		}
 	}
+
+	public void put(DbxClient client) throws DbxException, IOException {
+		ByteArrayInputStream in = new ByteArrayInputStream(content);
+		client.uploadFile(path + name + "." + ext, DbxWriteMode.add(), -1, in);
+		return;
+	}
+
+}
