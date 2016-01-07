47dfef51d472850ad588be0b2285dc1ad2a88ae0
madsmai <mads.e.maibohm@gmail.com>
2016-01-07 20:18:38 +0100

    Pull from tag

--- Hello02148/src/ImgurConnecter.java
+++ Hello02148/src/ImgurConnecter.java
@@ -14,65 +14,80 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.ArrayList;
 import java.util.Base64;
+import java.util.List;
 
 import javax.imageio.ImageIO;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.tidy.Tidy;
+
+
 public class ImgurConnecter {
  	public static void main(String[] args)  {
-		File file = new File("C:\\Users\\Mads\\Pictures\\beta.PNG");
-		try {
-			System.out.println(uploadToImgur(file));
+//		File file = new File("C:\\Users\\Mads\\Pictures\\beta.PNG");
+//		System.out.println(uploadToImgur(file));
+//		String image = "http://i.imgur.com/TJRrIdJ.jpg";
+//		downloadFromImgur(image);
+ 		List<String> images = getImgByTag("http://imgur.com/t/selfie");
+ 		for(String img: images) {
+ 			System.out.println(img);
+ 		}
+		
+
+	}
+	public static String uploadToImgur(File file) {
+
+	    BufferedImage image = null;
+	    URL url;
+	    String output = "upload failed";
+	    //Reading and preparing the image in base64
+	    try {
+			image = ImageIO.read(file);
+			//Encoding the image 
+		    ByteArrayOutputStream byteArray = new ByteArrayOutputStream();
+		    ImageIO.write(image, "png", byteArray);
+		    byte[] byteImage = byteArray.toByteArray();
+		    		//String dataImage = Base64.encode(byteImage);
+		    String dataImage = Base64.getEncoder().encodeToString(byteImage);
+		    String data = URLEncoder.encode("image", "UTF-8") + "="
+		    + URLEncoder.encode(dataImage, "UTF-8");
+		    
+		    url = new URL("https://api.imgur.com/3/image");
+		    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		    conn.setDoOutput(true);
+		    conn.setDoInput(true);
+		    conn.setRequestMethod("POST");
+		    conn.setRequestProperty("Authorization", "Client-ID " + "b34d2583bb8a43f");
+		    conn.setRequestMethod("POST");
+		    conn.setRequestProperty("Content-Type",
+		            "application/x-www-form-urlencoded");
+
+		    conn.connect();
+		    StringBuilder stb = new StringBuilder();
+		    OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+		    wr.write(data);
+		    wr.flush();
+
+		    // Get the response
+		    BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+		    String line;
+		    while ((line = rd.readLine()) != null) {
+		        stb.append(line).append("\n");
+		    }
+		    wr.close();
+		    rd.close();
+		    output=stb.toString();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		String image = "http://i.imgur.com/TJRrIdJ.jpg";
-		downloadFromImgur(image);
-
-	}
-	public static String uploadToImgur(File file) throws IOException {
-
-	    BufferedImage image = null;
-	    URL url;
-	    //Reading and preparing the image in base64
-	    image = ImageIO.read(file);
 	    
-	    //Encoding the image 
-	    ByteArrayOutputStream byteArray = new ByteArrayOutputStream();
-	    ImageIO.write(image, "png", byteArray);
-	    byte[] byteImage = byteArray.toByteArray();
-	    		//String dataImage = Base64.encode(byteImage);
-	    String dataImage = Base64.getEncoder().encodeToString(byteImage);
-	    String data = URLEncoder.encode("image", "UTF-8") + "="
-	    + URLEncoder.encode(dataImage, "UTF-8");
 	    
-	    url = new URL("https://api.imgur.com/3/image");
-	    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-	    conn.setDoOutput(true);
-	    conn.setDoInput(true);
-	    conn.setRequestMethod("POST");
-	    conn.setRequestProperty("Authorization", "Client-ID " + "b34d2583bb8a43f");
-	    conn.setRequestMethod("POST");
-	    conn.setRequestProperty("Content-Type",
-	            "application/x-www-form-urlencoded");
 
-	    conn.connect();
-	    StringBuilder stb = new StringBuilder();
-	    OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
-	    wr.write(data);
-	    wr.flush();
-
-	    // Get the response
-	    BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-	    String line;
-	    while ((line = rd.readLine()) != null) {
-	        stb.append(line).append("\n");
-	    }
-	    wr.close();
-	    rd.close();
-
-	    return stb.toString();
+	    return output;
 	}
 	public static String downloadFromImgur(String imageURL) {
 		URL url;
@@ -108,5 +123,35 @@ public class ImgurConnecter {
 		return null;
 		
 	}
+	public static List<String> getImgByTag(String tagURL) {
+		InputStream input;
+		List<String> srcs=null;
+		try {
+			input = new URL(tagURL).openStream();
+			Tidy tidy = new Tidy();
+			tidy.setShowErrors(0);
+			tidy.setShowWarnings(false);;
+			//tidy.setErrout(null);
+			Document document = tidy.parseDOM(input, null);
+			NodeList imgs = document.getElementsByTagName("img");
+			srcs = new ArrayList<String>();
+
+			for (int i = 0; i < imgs.getLength(); i++) {
+			    srcs.add(imgs.item(i).getAttributes().getNamedItem("src").getNodeValue());
+			}
+			for(int i=0; i<srcs.size(); i++) {
+				srcs.set(i, srcs.get(i).substring(0, srcs.get(i).length()-5) + srcs.get(i).substring(srcs.get(i).length()-4));
+			}
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return srcs;
+	}
+
 }
 
